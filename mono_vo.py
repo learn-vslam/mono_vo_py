0fb299d22166253004ba7b8c66815e3837f8f8a4
JeffreyYH <jeffrey.yafei.hu@gmail.com>
2026-02-22 21:57:07 -0500

    [update]

--- mono_vo.py
+++ mono_vo.py
@@ -38,7 +38,8 @@ class CoordTransform:
 
 
 class Visualizer:
-    def __init__(self):
+    def __init__(self, config=None):
+        self.config = config
         self.server = viser.ViserServer()
 
         self.server.scene.add_frame(
@@ -46,32 +47,31 @@ class Visualizer:
             axes_length = 5,
             axes_radius = 0.1,
         )
-        
-        self.pcd_handle = self.server.scene.add_point_cloud(
-            name="/world_frame/point_cloud",
-            points=np.empty((0, 3)),
-            colors=np.empty((0, 3)),
-            point_size=0.005,
-        )
 
         self.batched_xyz = []
         self.batched_wxyz = []
-        if config.load_gt_pose:
+        if self.config and self.config.load_gt_pose:
             self.batched_xyz_gt = []
             self.batched_wxyz_gt = []
     
 
-    def add_cam_frame(self, T_wc, frame_name):
-        # change the world frame to ROS convention
-        T_viz = CoordTransform.T_cv_to_ros @ T_wc @ np.eye(4).T
-        
+    def get_pcd_handle(self, pcd_name):
+        return self.server.scene.add_point_cloud(
+            name=f"/world_frame/{pcd_name}",
+            points=np.empty((0, 3)),
+            colors=np.empty((0, 3)),
+            point_size=0.005,
+        )
+    
+
+    def add_cam_frame(self, T_wc, frame_names, frame_idx):
         # add camera coord frame
-        tx, ty, tz = T_viz[:3, 3]
-        qx, qy, qz, qw = Rotation.from_matrix(T_viz[:3, :3]).as_quat()
+        tx, ty, tz = T_wc[:3, 3]
+        qx, qy, qz, qw = Rotation.from_matrix(T_wc[:3, :3]).as_quat()
         self.batched_xyz.append((tx, ty, tz))
         self.batched_wxyz.append((qw, qx, qy, qz))
         self.server.scene.add_batched_axes(
-            f"/world_frame/{frame_name}",
+            f"/world_frame/{frame_names}/cam_{frame_idx}",
             axes_length = 1,
             axes_radius = 0.1,
             batched_wxyzs=self.batched_wxyz,
@@ -79,10 +79,7 @@ class Visualizer:
         )
     
 
-    def add_cam_frustum(self, curr_img, K, T_wc, frame_idx):
-        # change the world frame to ROS convention
-        T_viz = CoordTransform.T_cv_to_ros @ T_wc @ np.eye(4).T
-
+    def add_cam_frustum(self, curr_img, K, T_wc, frustum_names, frame_idx):
         # convert to RGB only for Viser display
         curr_img_rgb = cv2.cvtColor(curr_img, cv2.COLOR_BGR2RGB)
 
@@ -90,14 +87,14 @@ class Visualizer:
         fov = 2 * np.arctan2(curr_img_rgb.shape[0] / 2, K[0, 0])
         aspect = curr_img_rgb.shape[1] / curr_img_rgb.shape[0]
         downsample_factor = 1
-        self.visualizer.server.scene.add_camera_frustum(
-            f"/world_frame/cam_frustum/cam_{frame_idx}",
+        self.server.scene.add_camera_frustum(
+            f"/world_frame/{frustum_names}/cam_{frame_idx}",
             fov=fov,
             aspect=aspect,
             scale=1,
             image=curr_img_rgb[::downsample_factor, ::downsample_factor],
-            wxyz=tf.SO3.from_matrix(T_viz[:3, :3]).wxyz,
-            position=T_viz[:3, 3],
+            wxyz=tf.SO3.from_matrix(T_wc[:3, :3]).wxyz,
+            position=T_wc[:3, 3],
         )
 
 
@@ -117,9 +114,10 @@ class Visualizer:
 
 
 class MonoVO:
-    def __init__(self):
+    def __init__(self, config):
+        self.config = config
+        
         # dataloader
-        config = Config()
         loader_classes = {
             'KITTI': KITTILoader,
             'EuRoC': EuRoCLoader,
@@ -136,14 +134,23 @@ class MonoVO:
         self.K = np.array([[cam_config['intrinsics']['fx'], 0, cam_config['intrinsics']['cx']],
                            [0, cam_config['intrinsics']['fy'], cam_config['intrinsics']['cy']],
                            [0, 0, 1]])
-        if config.load_gt_pose:
+        if self.config.load_gt_pose:
             self.gt_pose = loader.load_gt_pose()
+
+        # predicted poses and points
+        self.T_wc_list = []
+        self.pcd_all = []
+        self.pcd_colors_all = []
         
         # output pose file
-        output_dir = os.path.join(config.result_dir, config.dataset_type)
+        output_dir = os.path.join(self.config.result_dir, self.config.dataset_type)
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
-        self.out_pose_file = os.path.join(output_dir, config.seq + '-traj_est.txt')
+        self.out_pose_file = os.path.join(output_dir, self.config.seq + '-traj_est.txt')
+        
+        # initialize visualizer
+        self.visualizer = Visualizer(self.config)
+        self.pcd_handle = self.visualizer.get_pcd_handle("point_cloud")
 
 
     def extract_match_features(self, prev_img, curr_img, n_feats=10000, matcher_type='bf', show_matches=True):
@@ -191,12 +198,14 @@ class MonoVO:
         T_cw = np.eye(4)
         T_cw[0:3, 0:3] = R_cw
         T_cw[0:3, 3] = t_cw.flatten()
-        
+
         return np.linalg.inv(T_cw)
 
 
     def triangulate(self, kp1, kp2, K, T_1w, T_2w):
-        """Triangulate 3D points given two camera poses and keep only points in front of both cameras."""
+        """
+        Triangulate 3D points given two camera poses and keep only points in front of both cameras
+        """
         # Compute projection matrices
         P1 = K @ np.linalg.inv(T_1w)[:3, :]  # Intrinsics * Extrinsics
         P2 = K @ np.linalg.inv(T_2w)[:3, :]
@@ -217,86 +226,116 @@ class MonoVO:
         valid_mask = (X_cam1[2, :] > 0) & (X_cam2[2, :] > 0)
 
         # Optional depth clamp in second camera frame
-        if config.max_depth is not None:
+        if self.config.max_depth is not None:
             depth = X_cam2[2, :]
-            valid_mask = valid_mask & (depth < config.max_depth)
+            valid_mask = valid_mask & (depth < self.config.max_depth)
 
         return X[:, valid_mask], valid_mask
     
 
+    def save_output_pose(self, frame_idx, T_wc):
+        curr_R_wc = T_wc[:3, :3]
+        curr_t_wc = T_wc[:3, 3].reshape(3, 1)
+        [qx, qy, qz, qw] = Rotation.from_matrix(curr_R_wc).as_quat()        
+        with open(self.out_pose_file, 'a') as f:
+            f.write('%f %f %f %f %f %f %f %f\n' % (frame_idx, 
+                                                    curr_t_wc.flatten()[0], 
+                                                    curr_t_wc.flatten()[1], 
+                                                    curr_t_wc.flatten()[2],
+                                                    qx, qy, qz, qw))
+    
+
+    def mapping_points(self, pts, pcd, curr_img):
+        """ 
+        get 3D points and valid mask and register points in world frame (ROS convention)
+        """
+        # sample RGB colors for the matched pixels in the current image and register points
+        u = np.clip(pts[:, 0].astype(int), 0, curr_img.shape[1]-1)
+        v = np.clip(pts[:, 1].astype(int), 0, curr_img.shape[0]-1)
+        colors_rgb = curr_img[v, u, ::-1] / 255.0  # BGR->RGB to [0,1]
+
+        pcd = np.concatenate((pcd, np.ones((1, pcd.shape[1]))), axis=0)
+        pcd = CoordTransform.T_cv_to_ros @ pcd
+        pcd = pcd[:3, :].T
+
+        self.pcd_all.extend(pcd.tolist())
+        self.pcd_colors_all.extend(colors_rgb.tolist())
+
+
+    def meet_keyframe_criteria(self, pts1, pts2, T_cc, min_parallax=1.0, min_translation=0.1):
+        """Select keyframes based on a few criteria"""
+
+        # enough baseline for good triangulation
+        translation = np.linalg.norm(T_cc[:3, 3])
+        if translation < min_translation:
+            enough_baseline = False
+        else:
+            enough_baseline = True
+        
+        # sufficient parallax between matched points
+        median_parallax = np.median(np.linalg.norm(pts2 - pts1, axis=1))
+        if median_parallax < min_parallax:
+            sufficient_parallax = False  
+        else:
+            sufficient_parallax = True
+                
+        return enough_baseline and sufficient_parallax
+
+
     def run(self):
-        T_wc_list = []
-        pcd_all = []
-        pcd_colors_all = []
+        for frame_idx in range(self.num_frames):
+            curr_img = cv2.imread(self.img_files_list[frame_idx], 1)
+            if self.config.load_gt_pose:
+                curr_T_gt = self.gt_pose[frame_idx]
 
-        for i in range(self.num_frames):
-            curr_img = cv2.imread(self.img_files_list[i], 1)
-            if config.load_gt_pose:
-                curr_T_gt = self.gt_pose[i]
-
-            if i == 0:
+            if frame_idx == 0:
                 curr_T_wc = np.eye(4)
+                curr_T_wc_ros = CoordTransform.T_cv_to_ros @ curr_T_wc @ np.eye(4).T
+                is_kf = True
             else:
-                # prev_imgName = img_data_dir + str(i - 1).zfill(6) + '.png'
-                prev_img = cv2.imread(self.img_files_list[i-1], 0)
+                prev_img = cv2.imread(self.img_files_list[frame_idx-1], 0)
 
                 # feature extraction and matching, then compute relative pose in cam frame 
-                pts1, pts2 = self.extract_match_features(prev_img, curr_img, matcher_type='flann', show_matches=False)
-                T_cc = self.compute_pose(pts1, pts2)
+                prev_kps, curr_kps = self.extract_match_features(prev_img, curr_img, matcher_type='flann', show_matches=False)
+                T_cc = self.compute_pose(prev_kps, curr_kps)
 
                 # get current pose
                 curr_T_wc = prev_T_wc @ T_cc
-                T_wc_list.append(curr_T_wc)
+                self.T_wc_list.append(curr_T_wc)
+                curr_T_wc_ros = CoordTransform.T_cv_to_ros @ curr_T_wc @ np.eye(4).T
                         
                 # triangulate only in key frames
-                if i%config.key_frame_interval == 0:
-                    pcd, valid_mask = self.triangulate(pts1, pts2, self.K, prev_T_wc, curr_T_wc)
+                is_kf = self.meet_keyframe_criteria(prev_kps, curr_kps, T_cc)
+                if is_kf:
+                    # get 3D points and valid mask
+                    pcd, valid_mask = self.triangulate(prev_kps, curr_kps, self.K, prev_T_wc, curr_T_wc)
                     if not np.any(valid_mask):
                         continue
-                    pts2 = pts2[valid_mask]
-
-                    # sample RGB colors for the matched pixels in the current image
-                    u = np.clip(pts2[:, 0].astype(int), 0, curr_img.shape[1]-1)
-                    v = np.clip(pts2[:, 1].astype(int), 0, curr_img.shape[0]-1)
-                    colors_rgb = curr_img[v, u, ::-1] / 255.0  # BGR->RGB to [0,1]
-
-                    pcd = np.concatenate((pcd, np.ones((1, pcd.shape[1]))), axis=0)
-                    pcd = CoordTransform.T_cv_to_ros @ pcd
-                    pcd = pcd[:3, :].T
-
-                    pcd_all.extend(pcd.tolist())
-                    pcd_colors_all.extend(colors_rgb.tolist())
+                    curr_kps = curr_kps[valid_mask]
+                    self.mapping_points(curr_kps, pcd, curr_img)
 
                     # update point cloud handle (avoids re-adding every time)
-                    self.visualizer.pcd_handle.points = np.array(pcd_all)
-                    self.visualizer.pcd_handle.colors = np.array(pcd_colors_all)
+                    self.pcd_handle.points = np.array(self.pcd_all)
+                    self.pcd_handle.colors = np.array(self.pcd_colors_all)
 
                 # draw the current image with keypoints
-                keypoints = [cv2.KeyPoint(x=pt[0], y=pt[1], size=1) for pt in pts2]
+                keypoints = [cv2.KeyPoint(x=pt[0], y=pt[1], size=1) for pt in curr_kps]
                 curr_img_kp = cv2.drawKeypoints(curr_img, keypoints, None, color=(0, 255, 0), flags=0)
                 cv2.imshow('keypoints from current image', curr_img_kp)
                 cv2.waitKey(1)
 
             # save current trajectory
-            curr_R_wc = curr_T_wc[:3, :3]
-            curr_t_wc = curr_T_wc[:3, 3].reshape(3, 1)
-            [qx, qy, qz, qw] = Rotation.from_matrix(curr_R_wc).as_quat()        
-            with open(self.out_pose_file, 'a') as f:
-                f.write('%f %f %f %f %f %f %f %f\n' % (i, 
-                                                       curr_t_wc.flatten()[0], 
-                                                       curr_t_wc.flatten()[1], 
-                                                       curr_t_wc.flatten()[2],
-                                                       qx, qy, qz, qw))
+            self.save_output_pose(frame_idx, curr_T_wc_ros)
 
             # update prev pose
             prev_T_wc = curr_T_wc
-            if config.load_gt_pose:
+            if self.config.load_gt_pose:
                 prev_T_gt = curr_T_gt
 
             # add camera frames and frustum for visualization
-            if i % config.key_frame_interval == 0:
-                self.visualizer.add_cam_frame(curr_T_wc, f"cam_{i}")
-                self.visualizer.add_cam_frustum(curr_img, self.K, curr_T_wc, i)
+            if is_kf:
+                self.visualizer.add_cam_frame(curr_T_wc_ros, "cam_frames", frame_idx)
+                self.visualizer.add_cam_frustum(curr_img, self.K, curr_T_wc_ros, "cam_frustum", frame_idx)
                 if config.load_gt_pose:
                     self.visualizer.add_gt_frame(curr_T_gt)
 
@@ -305,6 +344,5 @@ class MonoVO:
 
 if __name__ == "__main__":
     config = tyro.cli(Config)
-    MonoVO().run()
-    while True:
-        time.sleep(1)
+    MonoVO(config).run()
+    input("Press Enter to stop...")
